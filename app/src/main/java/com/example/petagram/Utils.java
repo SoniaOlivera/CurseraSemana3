d3bf7f355b3b2c975808582bbf12546f0a91f09f
SoniaOlivera <61550104+SoniaOlivera@users.noreply.github.com>
2020-11-02 21:55:47 -0300

    Commit
    Commit

--- app/src/main/java/com/example/petagram/Utils.java
+++
@@ -1,6 +0,0 @@
-package com.example.petagram;
-
-public class Utils {
-    public static final String EMAIL = "courserabit06@gmail.com";
-    public static final String PASSWORD = "prueba123";
-}
--- app/src/main/java/com/example/petagram/presentador/IRecyclerViewFragmentPresenter.java
+++
@@ -1,8 +0,0 @@
-package com.example.petagram.presentador;
-
-public interface IRecyclerViewFragmentPresenter {
-
-    public void obtenerMascotasBaseDatos();
-
-    public void mostrarContactosRV();
-}
